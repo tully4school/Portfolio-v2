fd14e2eb539bbe020032eca98b89648515d66a4a
Joseph_Tully <tully4school@gmail.com>
2020-01-12 20:37:24 -0500

    updating padding on container-fluid

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -3,7 +3,7 @@ import "./Container.css";
 class Container extends Component {
 	render() {
 		return (
-			<div className='container-fluid'>
+			<div className='container-fluid px-0'>
 				<div className={this.props.classes}>{this.props.children}</div>
 			</div>
 		);
